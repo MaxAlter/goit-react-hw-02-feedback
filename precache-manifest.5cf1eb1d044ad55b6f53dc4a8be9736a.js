8dfc25a0468f572a646798edb38a545091bcd232
MaxAlter <alter0075926@gmail.com>
2020-04-25 15:08:46 +0300

    Updates

---
+++ precache-manifest.5cf1eb1d044ad55b6f53dc4a8be9736a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953951161115401be36ec6586fb60c54",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "b559f9f2aa634438e706",
+    "url": "/goit-react-hw-02-feedback/static/css/main.3d540b33.chunk.css"
+  },
+  {
+    "revision": "1fd64ad4f6cf2670875e",
+    "url": "/goit-react-hw-02-feedback/static/js/2.dd22f636.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.dd22f636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b559f9f2aa634438e706",
+    "url": "/goit-react-hw-02-feedback/static/js/main.eb13b65a.chunk.js"
+  },
+  {
+    "revision": "004ab56f9e0334df59f4",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.2dc8ed6c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-feedback/precache-manifest.2d8f0e91a6e346529ea4eb89a2693675.js"
+  "/goit-react-hw-02-feedback/precache-manifest.5cf1eb1d044ad55b6f53dc4a8be9736a.js"
 );
 
 self.addEventListener('message', (event) => {
